fbb0fdde1c45ce9fda18507b6fb023a11096762f
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-11-29 22:02:01 +0300

    1

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        trueOrFalse(inputUser());
+    }
+    public static int inputUser(){
+        System.out.println("Введите целое число: ");
+        return new Scanner(System.in).nextInt();
+    }
+    public static void trueOrFalse(int num){
+        if (num%2==0){
+            System.out.println("true");
+        }else System.out.println("false");
     }
 }
